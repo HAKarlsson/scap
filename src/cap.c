3fbf6fef93ddebe7287cf4f5c42613146d0ce73f
Henrik Karlsson <henrik10@kth.se>
2024-07-18 10:55:37 +0200

    Adds comments to cap.c

--- src/cap.c
+++ src/cap.c
@@ -1,6 +1,9 @@
 #include "cap.h"
 #include <stdint.h>
 
+/**
+ * Basic memory capability
+ */
 typedef struct {
 	int8_t pid;
 	uint8_t fuel;
@@ -8,59 +11,85 @@ typedef struct {
 	uint8_t rwx;
 	uint32_t base;
 	uint32_t size;
-} Cap;
+} cap_t;
 
-extern Cap ctable[];
+// Capability table
+cap_t ctable[64] = {
+	// Initial capability
+	{ .pid = 0,
+	  .fuel = 64,
+	  .max_fuel = 64,
+	  .rwx = 0x7,
+	  .base = 0x80000000,
+	  .size = 0x10000
+	}
+};
 
-int CapDerive(int pid, int i, uint8_t fuel, uint32_t base, uint32_t size, uint8_t rwx)
+int cap_derive(int pid, int i, uint8_t fuel, uint32_t base, uint32_t size, uint8_t rwx)
 {
-	Cap *cap = &ctable[i];
+	cap_t *cap = &ctable[i];
 
-	if (i >= ARRAY_SIZE(ctable))
+	// Check capability index
+	if (i < 0 || i >= ARRAY_SIZE(ctable))
 		return -1;
+	// Check owner
 	if (cap->pid != pid)
 		return -1;
+	// Check if sufficient fuel
 	if (cap->fuel < fuel)
 		return -1;
-	if (base < cap->base)
+	// Check if out of bounds
+	if ((base < cap->base) || (base + size > cap->base + cap->size))
 		return -1;
-	if (base + size > cap->base + cap->size)
+	// Check RWX permissions. 
+	if ((cap->rwx & rwx) != rwx)
 		return -1;
 
-	// Index of new capability
+	// Index of new capability.
 	int j = i + cap->fuel - fuel;
 
+	// Create new capability.
 	ctable[j].pid = ctable[i].pid;
 	ctable[j].fuel = fuel;
 	ctable[j].max_fuel = fuel;
 	ctable[j].base = base;
 	ctable[j].size = size;
 
+	// Return index of new capability.
 	return j;
 }
 
-void CapRevoke(int pid, int i)
+void cap_revoke(int pid, int i)
 {
-	if (i >= ARRAY_SIZE(ctable))
+	cap_t *cap = &ctable[i];
+
+	// Check capability index.
+	if (i < 0 || i >= ARRAY_SIZE(ctable))
 		return -1;
+
+	// Check owner.
 	if (cap->pid != pid)
 		return -1;
-	Cap *cap = &ctable[i];
-	Cap *bgn = &ctable[i + cap->fuel];
-	Cap *end = &ctable[i + cap->max_fuel];
 
-	while (bgn != end)
-		(bgn++).pid = -1;
+	// Invalidate child capabilities.
+	for (int j = cap->fuel; j < cap->max_fuel; ++j)
+		ctable[i + j].pid = -1;
 
+	// Restore fuel.
 	cap->fuel = cap->max_fuel;
 }
 
-void CapDelete(int pid, int i)
+void cap_delete(int pid, int i)
 {
+	cap_t *cap = &ctable[i];
+
+	// Check capability index.
 	if (i >= ARRAY_SIZE(ctable))
 		return -1;
+	// Check owner.
 	if (cap->pid != pid)
 		return -1;
-	Cap *cap = &ctable[i];
+
+	// Invalidate capability.
 	cap->pid = -1;
 }
